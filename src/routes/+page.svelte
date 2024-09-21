c42dff7522c5d1057e4cbb0a3d48e184ad829a81
Adam Weyrah <adam@Adams-MacBook-Pro.local>
2024-09-20 18:48:53 -0600

    setup dark mode toggle with

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+      import { toggleMode } from "mode-watcher";
+</script>
+
+<div class="w-full h-full flex items-center justify-center">
+    <button onclick={toggleMode}>toggle mode</button>
+</div>
