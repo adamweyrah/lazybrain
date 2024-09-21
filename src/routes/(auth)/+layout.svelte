7512171756efdb9662b547096d2436f7cbab8dcd
Adam Weyrah <adam@Adams-MacBook-Pro.local>
2024-09-20 22:47:06 -0600

    added shadcn-svelte

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,3 @@
+<main class="flex h-full w-full items-center justify-center">
+	<slot />
+</main>
---
+++ src/routes/(auth)/register/+page.svelte
@@ -0,0 +1,6 @@
+<div class="rounded border p-4">
+	<div class="flex flex-col">
+		<h1>🧠 LazyBrain</h1>
+		<p>A dead simple productivty app to get you started.</p>
+	</div>
+</div>
